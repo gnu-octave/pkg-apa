e4fe8f5e4b9c910a6a1d10f23d559005ed5c9ee5
Kai T. Ohlhus <k.ohlhus@gmail.com>
2021-07-05 17:40:23 +0900

    Add ideas for MPFR memory management

--- src/@mp/private/__mpfr__.c
+++ src/@mp/private/__mpfr__.c
@@ -4,6 +4,79 @@
 #include "mex.h"
 #include "mpfr.h"
 
+#define DATA_CHUNK_SIZE 1000  // Allocate space for MPFR variables in chunks.
+
+// MPFR memory management
+// ======================
+//
+// Similar to C++ std::vector:
+// - data_capacity: number of elements that `data` has currently allocated
+//                  space for.
+// - data_size:     number of elements in `data`.
+
+static mpfr_ptr data = NULL;
+static size_t data_capacity = 0;
+static size_t data_size = 0;
+
+
+// Function called at exit of the mex file to tidy up all memory.
+
+static void
+mpfr_tidy_up (void)
+{
+  mexPrintf ("__mpfr__(mpfr_tidy_up): Call.\n");
+  for (size_t i = 0; i < data_size; i++)
+    mpfr_clear (&data[i]);
+  mxFree (data);
+}
+
+
+// Constructor for new MPFR variables.
+//
+// @returns 1-based start and end index of the MPFR variables `idx_start` and
+//          `idx_end`.
+
+int mpfr_create (size_t count, mpfr_prec_t prec,
+                 size_t* idx_start, size_t* idx_end)
+{
+  mexPrintf ("__mpfr__(mpfr_create): Call.\n");
+  // Check if there is enough space to create new MPFR variables.
+  if ((data_size + count) > data_capacity)
+    {
+      // Determine new capacity.
+      size_t data_new_capacity = data_capacity;
+      while ((data_size + count) > data_new_capacity)
+        data_new_capacity += DATA_CHUNK_SIZE;
+
+      mexPrintf ("__mpfr__(mpfr_create): Increase capacity to '%d'\n",
+        data_new_capacity);
+      // Realloate memory.
+      if (data == NULL)
+        {
+          data = (mpfr_ptr) mxMalloc (data_new_capacity * sizeof (mpfr_t));
+          mexAtExit (mpfr_tidy_up);
+        }
+      else
+        data = (mpfr_ptr) mxRealloc (data, data_new_capacity * sizeof (mpfr_t));
+      if (data == NULL)
+        return 0;  // Memory allocation failed.
+      mexMakeMemoryPersistent (data);
+      data_capacity = data_new_capacity;
+    }
+
+  // Initialize new MPFR variables and increase number of elements in `data`.
+  for (size_t i = 0; i < count; i++)
+    mpfr_init2 (&data[data_size + i], prec);
+  
+  *idx_start = data_size + 1;
+  data_size += count;
+  *idx_end = data_size;
+  mexPrintf ("__mpfr__(mpfr_create): New MPFR variable(s) '%d:%d'"
+    " (prec = %d)\n", *idx_start, *idx_end, (int) prec);
+
+  return 1;
+}
+
 
 // Rounding mode translation
 // =========================
@@ -41,8 +114,8 @@ rounding_mode_double2mpfr (double dbl_rnd_mode)
         break;
       default:
         mexPrintf ("__mpfr__: Invalid rounding mode '%d'.  "
-          "Using round to nearest 'MPFR_RNDN'.\n", (int) dbl_rnd_mode);
-        rnd_mode = MPFR_RNDN;
+          "Using default rounding mode.\n", (int) dbl_rnd_mode);
+        rnd_mode = mpfr_get_default_rounding_mode ();
         break;
     }
   return rnd_mode;
@@ -78,6 +151,8 @@ rounding_mode_mpfr2double (mpfr_rnd_t mpfr_rnd_mode)
 }
 
 
+// Safely convert a double to MPFR precision.
+
 mpfr_prec_t
 double2prec (double d)
 {
@@ -86,7 +161,7 @@ double2prec (double d)
   if (mxIsFinite (d) && (p > MPFR_PREC_MIN) && (p < MPFR_PREC_MAX))
     return p;
 
-  mexPrintf ("__mpfr__: Precision must be a numeric scalar beween %d and %d.\n",
+  mexPrintf ("__mpfr__: Precision must be a numeric scalar beween %ld and %ld.\n",
              MPFR_PREC_MIN, MPFR_PREC_MAX);
   return mpfr_get_default_prec ();
 }
@@ -102,7 +177,6 @@ mexFunction (int nlhs, mxArray *plhs[],
   if ((nrhs < 1) || ! mxIsChar (prhs[0])) {
     mexErrMsgIdAndTxt ("mp:mex:rhs", "First input must be a string.");
   }
-
   // Read command to execute.
   size_t buf_len = (mxGetM (prhs[0]) * mxGetN (prhs[0])) + 1;
   char *cmd_buf = mxCalloc (buf_len, sizeof (char));
@@ -110,8 +184,7 @@ mexFunction (int nlhs, mxArray *plhs[],
 
   mexPrintf("Command: '%s'\n", cmd_buf);
   
-  if (strncmp (cmd_buf, "get_default_prec",
-                strlen ("get_default_prec")) == 0)
+  if (strcmp (cmd_buf, "get_default_prec") == 0)
     plhs[0] = mxCreateDoubleScalar ((double) mpfr_get_default_prec ());
   else if (strncmp (cmd_buf, "set_default_prec",
                      strlen ("set_default_prec")) == 0)
@@ -122,12 +195,10 @@ mexFunction (int nlhs, mxArray *plhs[],
         mexPrintf ("__mpfr__(%s): Precision must be a numeric scalar.\n",
                    cmd_buf);
     }
-  else if (strncmp (cmd_buf, "get_default_rounding_mode",
-                     strlen ("get_default_rounding_mode")) == 0)
+  else if (strcmp (cmd_buf, "get_default_rounding_mode") == 0)
     plhs[0] = mxCreateDoubleScalar (
                 rounding_mode_mpfr2double (mpfr_get_default_rounding_mode ()));
-  else if (strncmp (cmd_buf, "set_default_rounding_mode",
-                     strlen ("set_default_rounding_mode")) == 0)
+  else if (strcmp (cmd_buf, "set_default_rounding_mode") == 0)
     {
       if ((nrhs == 2) && mxIsScalar (prhs[1]) && mxIsNumeric (prhs[1]))
         mpfr_set_default_rounding_mode (
@@ -136,16 +207,95 @@ mexFunction (int nlhs, mxArray *plhs[],
         mexPrintf ("__mpfr__(%s): Rounding mode must be a numeric scalar.\n",
                    cmd_buf);
     }
-  else if (strncmp (cmd_buf, "mp", strlen ("mp")) == 0)
+  else if (strcmp (cmd_buf, "init2") == 0)
     {
-      if ((nrhs < 2) || (nrhs > 3))
-        mexPrintf ("__mpfr__(%s): Too few arguments.\n", cmd_buf);
+      if (nrhs != 3)
+        mexPrintf ("__mpfr__(%s): Invalid number of arguments.\n", cmd_buf);
+      else if (! (mxIsScalar (prhs[1]) && mxIsNumeric (prhs[1])))
+        mexPrintf ("__mpfr__(%s): Count must be a positive numeric scalar.\n",
+                   cmd_buf);
+      else if (! mxIsScalar (prhs[2]) || ! mxIsNumeric (prhs[2]))
+        mexPrintf ("__mpfr__(%s): Precision must be a numeric scalar.\n",
+                   cmd_buf);
       else
         {
-          mpfr_rnd_t rnd_mode = ((nrhs == 3) && mxIsScalar (prhs[2]))
-                              ? rounding_mode_double2mpfr (
-                                  mxGetScalar (prhs[2]))
-                              : mpfr_get_default_rounding_mode ();
+          mpfr_prec_t prec = double2prec (mxGetScalar (prhs[2]));
+          double dcount = mxGetScalar (prhs[1]);
+          size_t count = (size_t) dcount;
+          if ((count > 0) && (dcount > 0.0) && mxIsFinite (dcount))
+            {
+              size_t idx_start = 0;
+              size_t idx_end = 0;
+              if (mpfr_create (count, prec, &idx_start, &idx_end)
+                  && (idx_start > 0) && (idx_end >= idx_start)
+                  && ((idx_end - idx_start + 1) == count))
+                {
+                  plhs[0] = mxCreateNumericMatrix (count, 1, mxDOUBLE_CLASS,
+                                                   mxREAL);
+                  double* ptr = mxGetPr (plhs[0]);
+                  for (size_t i = 0; i < count; i++)
+                    ptr[i] = (double) idx_start++;
+                }
+              else
+                mexPrintf ("__mpfr__(%s): Memory allocation failed.\n",
+                           cmd_buf);
+            }
+          else
+            mexPrintf ("__mpfr__(%s): Count must be a positive numeric scalar.\n",
+                       cmd_buf);
+        }
+    }
+  else if (strcmp (cmd_buf, "data_capacity") == 0)
+    plhs[0] = mxCreateDoubleScalar ((double) data_capacity);
+  else if (strcmp (cmd_buf, "data_size") == 0)
+    plhs[0] = mxCreateDoubleScalar ((double) data_size);
+  else if (strcmp (cmd_buf, "set_d") == 0)
+    {
+      if (nrhs != 4)
+        mexPrintf ("__mpfr__(%s): Invalid number of arguments.\n", cmd_buf);
+      else if (! mxIsNumeric (prhs[1]))
+        mexPrintf ("__mpfr__(%s): Invalid MPFR variable indices.\n", cmd_buf);
+      else if (! mxIsScalar (prhs[3]) || ! mxIsNumeric (prhs[3]))
+        mexPrintf ("__mpfr__(%s): Rounding mode must be a numeric scalar.\n",
+                   cmd_buf);
+      else
+        {
+          double idx = mxGetScalar (prhs[1]);
+          size_t data_0_idx = ((size_t) idx) - 1;
+          double val = mxGetScalar (prhs[2]);
+          if ((data_size > data_0_idx) && (idx >= 1.0) && mxIsFinite (idx))
+            idx = mpfr_set_d (&data[data_0_idx], val,
+                              rounding_mode_double2mpfr (mxGetScalar (prhs[3])));
+              
+          else
+            mexPrintf ("__mpfr__(%s): Invalid MPFR variable indices.\n",
+                       cmd_buf);
+        }
+    }
+  else if (strcmp (cmd_buf, "get_d") == 0)
+    {
+      if (nrhs != 3)
+        mexPrintf ("__mpfr__(%s): Invalid number of arguments.\n", cmd_buf);
+      else if (! mxIsNumeric (prhs[1]))
+        mexPrintf ("__mpfr__(%s): Invalid MPFR variable indices.\n", cmd_buf);
+      else if (! mxIsScalar (prhs[2]) || ! mxIsNumeric (prhs[2]))
+        mexPrintf ("__mpfr__(%s): Rounding mode must be a numeric scalar.\n",
+                   cmd_buf);
+      else
+        {
+          double idx = mxGetScalar (prhs[1]);
+          size_t data_0_idx = ((size_t) idx) - 1;
+          if ((data_size > data_0_idx) && (idx >= 1.0) && mxIsFinite (idx))
+            idx = mpfr_get_d (&data[data_0_idx],
+                              rounding_mode_double2mpfr (mxGetScalar (prhs[2])));
+              
+          else
+            {
+              idx = FP_NAN;
+              mexPrintf ("__mpfr__(%s): Invalid MPFR variable indices.\n",
+                         cmd_buf);
+            }
+          plhs[0] = mxCreateDoubleScalar (idx);
         }
     }
   else
@@ -153,10 +303,3 @@ mexFunction (int nlhs, mxArray *plhs[],
 
   mxFree (cmd_buf);
 }
-
-
-//TODO: int mexAtExit(void (*ExitFcn)(void));
-//      void mexMakeMemoryPersistent(void *ptr);
-//      void mexLock(void);
-//      void mexUnlock(void);
-//      bool mexIsLocked(void);
--- src/@mp/private/test_mpfr.m
+++ src/@mp/private/test_mpfr.m
@@ -1,26 +1,52 @@
-clc;
 clear -f __mpfr__
 mkoctfile --mex -Wall -Wextra __mpfr__.c -lmpfr -lgmp
 
 % Good input
-for i = [4, 53]
-  __mpfr__ ("set_default_prec", i)
-  assert (__mpfr__ ("get_default_prec"), i)
+for i = [4, flintmax("double"), 53]
+  __mpfr__ ("set_default_prec", i);
+  assert (__mpfr__ ("get_default_prec"), i);
 end
 % Bad input
 __mpfr__ ("set_default_prec", 42)
-for i = {inf, -42, -2, 1/6, nan, 'c', eye(3)}
-  __mpfr__ ("set_default_prec", i{1})
-  assert (__mpfr__ ("get_default_prec"), 42)
+for i = {inf, -42, -2, 1/6, nan, 'c', eye(3), intmax("int64")}
+  __mpfr__ ("set_default_prec", i{1});
+  assert (__mpfr__ ("get_default_prec"), 42);
 end
 
 % Good input
 for i = -1:3
-  __mpfr__ ("set_default_rounding_mode", i)
-  assert (__mpfr__ ("get_default_rounding_mode"), i)
+  __mpfr__ ("set_default_rounding_mode", i);
+  assert (__mpfr__ ("get_default_rounding_mode"), i);
 end
 % Bad input
+__mpfr__ ("set_default_rounding_mode", 0);
 for i = {inf, -42, -2, 4, 1/6, nan, 'c', eye(3)}
-  __mpfr__ ("set_default_rounding_mode", i{1})
-  assert (__mpfr__ ("get_default_rounding_mode"), 0)
+  __mpfr__ ("set_default_rounding_mode", i{1});
+  assert (__mpfr__ ("get_default_rounding_mode"), 0);
+end
+
+% Good input
+DATA_CHUNK_SIZE = 1000;
+assert (__mpfr__ ("init2", 1, 50), 1);
+assert (__mpfr__ ("data_size"), 1);
+assert (__mpfr__ ("data_capacity"), DATA_CHUNK_SIZE);
+assert (__mpfr__ ("init2", DATA_CHUNK_SIZE, 75), (1:DATA_CHUNK_SIZE)' + 1);
+assert (__mpfr__ ("data_size"), DATA_CHUNK_SIZE + 1);
+assert (__mpfr__ ("data_capacity"), 2 * DATA_CHUNK_SIZE);
+% Bad input
+for i = {1/2, inf, -42, -1, -2, nan, 'c', eye(3)}
+  __mpfr__ ("init2", i{1}, 75)
+  assert (__mpfr__ ("data_size"), DATA_CHUNK_SIZE + 1);
+  assert (__mpfr__ ("data_capacity"), 2 * DATA_CHUNK_SIZE);
+end
+
+% Good input
+for i = {inf, -42, -2, nan, 1, 2.5, pi}
+  __mpfr__ ("set_d", 1, i{1}, 0)
+  assert (__mpfr__ ("get_d", 1, 0), i{1})
+end
+% Good input (with eps tolerance)
+for i = {1/6}
+  __mpfr__ ("set_d", 1, i{1}, 0)
+  assert (__mpfr__ ("get_d", 1, 0), i{1}, eps ())
 end
