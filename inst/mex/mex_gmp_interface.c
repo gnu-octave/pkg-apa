92c80e72c13d1bf481705b1126fe13b270825a14
Kai T. Ohlhus <k.ohlhus@gmail.com>
2026-03-07 21:40:17 +0900

    inst/mex/mex_gmp_interface.c: simplify and avoid compiler warnings, fixes #3

--- inst/mex/mex_gmp_interface.c
+++ inst/mex/mex_gmp_interface.c
@@ -32,7 +32,6 @@ mex_gmp_interface (int nlhs, mxArray *plhs[],
                    int nrhs, const mxArray *prhs[],
                    uint64_t cmd_code)
 {
-  nlhs += (prhs - prhs);  // FIXME: Avoid unused variable warning.
 
   #if (! defined(__GNU_MP_VERSION) || (__GNU_MP_VERSION < 6))
   # error "Oldest supported GMP version is 6."
@@ -43,15 +42,12 @@ mex_gmp_interface (int nlhs, mxArray *plhs[],
       case 3000:  // const char * const gmp_version
       {
         MEX_NARGINCHK (1);
-        char *output_buf = (char *) mxCalloc (strlen (gmp_version),
-                                              sizeof(char));
-        strcpy (output_buf, gmp_version);
-        plhs[0] = mxCreateString (output_buf);
+        plhs[0] = mxCreateString (gmp_version);
         return;
       }
 
       default:
-        MEX_FCN_ERR ("Unknown command code '%d'\n", cmd_code);
+        MEX_FCN_ERR ("Unknown command code '%d', nlhs = %d, nrhs = %d, ptr(prhs) = %p\n", (int) cmd_code, nlhs, nrhs, prhs);
     }
 }
 
